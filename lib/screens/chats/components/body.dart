4f6f55e6a5a840b8db88e2e6d3c232fef66e1e1a
shaxobiddin <shaxobiddins00@gmail.com>
2022-06-22 11:00:51 +0500

    Initial commit

---
+++ lib/screens/chats/components/body.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:telegram_clone/components/filled_outline_button.dart';
+import 'package:telegram_clone/constants.dart';
+import 'package:telegram_clone/models/Chat.dart';
+import 'package:telegram_clone/screens/messages/message_screen.dart';
+
+import 'chat_card.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          padding: EdgeInsets.fromLTRB(
+              kDefaultPadding, 0, kDefaultPadding, kDefaultPadding),
+          color: kPrimaryColor,
+          child: Row(
+            children: [
+              FillOutlineButton(press: () {}, text: "Recent Message"),
+              SizedBox(width: kDefaultPadding),
+              FillOutlineButton(
+                press: () {},
+                text: "Active",
+                isFilled: false,
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemCount: chatsData.length,
+            itemBuilder: (context, index) => ChatCard(
+              chat: chatsData[index],
+              press: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => MessagesScreen(),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
